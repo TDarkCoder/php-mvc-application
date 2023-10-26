b5a423695c5f1be5ed85973b01068c7313256b08
Azizbek Khalilov <azizbekkhalilov151@gmail.com>
2023-10-26 08:54:44 +0500

    Add github workflow

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,4 +6,12 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    private const DEFAULT_STRING = 'education';
+
+    public function testStringsEqual()
+    {
+        $string = self::DEFAULT_STRING;
+
+        $this->assertSame(self::DEFAULT_STRING, $string);
+    }
 }
